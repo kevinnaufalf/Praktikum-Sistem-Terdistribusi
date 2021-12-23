ded64d469e10569fefa6f155064e6634719e5a61
Kevin Naufal F <63483517+kevinnaufalf@users.noreply.github.com>
2021-12-23 17:04:27 +0700

    Add files via upload

---
+++ uas-api1/database/migrations/2021_12_21_085357_create_data_barangs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDataBarangsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data_barangs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('type');
+            $table->string('spek');
+            $table->string('jumlah');
+            $table->string('harga');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('data_barangs');
+    }
+}
